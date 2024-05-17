7a478d0470af1aa2d52f276499ab0f570ab74c51
LucioZampella <162721696+LucioZampella@users.noreply.github.com>
2024-05-17 11:21:45 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,20 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+	new_lista = []
+	index = 0
+	for  string in lista:
+		if string:
+			new_lista.append(f"{index}. {string}")
+			index += 1
+	return new_lista
 
+print(enumerate_list(["Red", "Green", "Blue", "", "Yellow"]))
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista):
+	new_lista = []
+	index = 0
+	for string in lista:
+		if string:
+			new_lista.append(f"{index}.{string[::-1]}")
+			index += 1
+	return new_lista
+print(enumerate_backwards(["Red", "Green", "Blue", "", "Yellow"]))
